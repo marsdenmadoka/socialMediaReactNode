96837e681b11012ef030a4a85f5d1ea53b69e10d
marsdenmadoka <marsmadoka98@gmail.com>
2021-05-06 21:45:52 +0300

    added remove likes and remove comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,7 +52,7 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-//@route GET api/post/:id
+//@route GET api/post/:postid
 //@desc  GET post by ID
 //@access private
 
@@ -72,7 +72,7 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
-//@route DELETE api/post/:id
+//@route DELETE api/post/:postid
 //@desc DELETE post by ID
 //@access private
 
@@ -96,6 +96,135 @@ router.delete("/:id", auth, async (req, res) => {
     if (err.kind === "ObjectId") {
       return res.status(404).json({ msg: "Post not found" });
     }
+  }
+});
+
+//@route PUT api/post/like/:postid
+//@desc LIKE A post
+//@access private
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check if the post has already been liked //user cannt like the same post twice
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "post already liked" });
+    }
+    //else save the like
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server Error");
+  }
+});
+
+//@route PUT api/post/unlike/:postid
+//@desc UNLIKE A post
+//@access private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check if the post has already been liked since we cannot unlikea post that we havent liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "post has not yet been liked" });
+    }
+
+    //remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server Error");
+  }
+});
+
+//@route Comment api/post/comment/:postid
+//@desc  comment on a post
+//@access private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
+
+//@route DELETE api/post/comment/:post_id/:comment_id
+//@desc delete a  comment
+//@access private
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //pull out the comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    //make sure comment exists
+    if (!comment) {
+      return res.status(400).json({ msg: "comment does not exist" });
+    }
+    //check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(400).json({ msg: "User not authorized" });
+    }
+
+    //remove index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
   }
 });
 
